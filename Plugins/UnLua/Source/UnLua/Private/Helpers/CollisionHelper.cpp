e03f98411b4ae266cc2340bf2a6da251a6ae58e1
unphics <481381521@qq.com>
2026-04-15 20:43:25 +0800

    目录: 文件迁移

--- Plugins/UnLua/Source/UnLua/Private/Helpers/CollisionHelper.cpp
+++ Plugins/UnLua/Source/UnLua/Private/Helpers/CollisionHelper.cpp
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and limitations under the License.
 
 #include "Engine/CollisionProfile.h"
-#include "CollisionHelper.h"
+#include "Helpers/CollisionHelper.h"
 
 TArray<FName> FCollisionHelper::ChannelNames;
 UEnum* FCollisionHelper::CollisionChannelEnum;
--- Plugins/UnLua/Source/UnLua/Private/Registries/EnumRegistry.cpp
+++ Plugins/UnLua/Source/UnLua/Private/Registries/EnumRegistry.cpp
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 // See the License for the specific language governing permissions and limitations under the License.
 
-#include "CollisionHelper.h"
+#include "Helpers/CollisionHelper.h"
 #include "EnumRegistry.h"
 #include "LowLevel.h"
 #include "LuaCore.h"
